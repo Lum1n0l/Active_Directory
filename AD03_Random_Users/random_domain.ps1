ff09f691b391d637cae6ab02dd51c9c2db423b79
Jake Parry <jakeparry@email.com>
2023-04-27 23:04:26 +0100

    Update

--- AD03_Random_Users/random_domain.ps1
+++ AD03_Random_Users/random_domain.ps1
@@ -1,13 +1,31 @@
 
-$group_names = (Get-Content "AD03_Random_Users/group_names.txt")
-$first_names = (Get-Content "AD03_Random_Users/first_names.txt")
-$surnames = (Get-Content "AD03_Random_Users/surnames.txt")
-$passwords = (Get-Content "AD03_Random_Users/passwords.txt")
+$group_names = [System.Collections.ArrayList](Get-Content "AD03_Random_Users/group_names.txt")
+$first_names = [System.Collections.ArrayList](Get-Content "AD03_Random_Users/first_names.txt")
+$surnames = [System.Collections.ArrayList](Get-Content "AD03_Random_Users/surnames.txt")
+$passwords = [System.Collections.ArrayList](Get-Content "AD03_Random_Users/passwords.txt")
 
 $groups = @()
+$users = @()
 
 $num_groups = 10
+for ( $i = 0; $i -lt $num_groups; $i++){
+    $new_group = (Get-Random -InputObject $group_names)
+    $groups += $new_group
+    $group_names.Remove($new_group)
+}
 
+$num_users = 100
+for ( $i = 0; $i -lt $num_users; $i++){
+    $first_name = (Get-Random -InputObject $first_names)
+    $surname = (Get-Random -InputObject $surnames)
+    $password = (Get-Random -InputObject $passwords)
+    $new_user = @{
+        "name"="$first_name $surname"
+        "password" = $password
+        "groups" = @( (Get-Random -InputObject $groups) ) 
+    }
 
-
-echo (Get-Random -InputObject $group_names) 
+    $first_names.Remove($first_name)
+    $surnames.Remove($surname)
+    $passwords.Remove($password)
+}
