36a1ff6438770ca451fa5232bb064e40f7647f9c
Jake Parry <jakeparry@email.com>
2023-04-30 02:04:09 +0100

    Change Min Password Length to 1

--- AD02_Automating_Users/gen_ad.ps1
+++ AD02_Automating_Users/gen_ad.ps1
@@ -45,7 +45,7 @@ function CreateADUser(){
 
 function WeakenPasswordPolicy(){
     secedit /export /cfg C:\Windows\Tasks\secpol.cfg
-    (Get-Content C:\Windows\Tasks\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\Windows\Tasks\secpol.cfg
+    (Get-Content C:\Windows\Tasks\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0").replace("MinimumPasswordLength = 7", "MinimumPasswordLength = 1") | Out-File C:\Windows\Tasks\secpol.cfg
     secedit /configure /db c:\windows\security\local.sdb /cfg c:\Windows\Tasks\secpol.cfg /areas SECURITYPOLICY
     Remove-Item -force C:\Windows\Tasks\secpol.cfg -confirm:$false
 }
